c86ac9220cd504b3f2754e88e14be90e7e575e6c
Yoany Mora <yoany.stephany@gmail.com>
2025-08-14 17:08:09 -0600

    [IMP] Fix inheritance All Page objects contain the Header component

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -1,10 +1,10 @@
-import { browser } from '@wdio/globals'
-
+import { browser } from '@wdio/globals';
+import Header from './header.component';
 /**
 * main page object containing all methods, selectors and functionality
 * that is shared across all page objects
 */
-export default class Page {
+export default class Page extends Header {
     /**
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
