43a433f6c2d24fb0f5fc7c781d8647c35d605497
Yoany Mora <yoany.stephany@gmail.com>
2025-08-14 11:41:01 -0600

    [IMP] Complete login and base page classes

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -8,8 +8,8 @@ class LoginPage extends Page {
     /**
      * define selectors using getter methods
      */
-    get inputUsername () {
-        return $('#username');
+    get inputEmail () {
+        return $('#email');
     }
 
     get inputPassword () {
@@ -24,8 +24,8 @@ class LoginPage extends Page {
      * a method to encapsule automation code to interact with the page
      * e.g. to login using username and password
      */
-    async login (username, password) {
-        await this.inputUsername.setValue(username);
+    async login (email, password) {
+        await this.inputEmail.setValue(email);
         await this.inputPassword.setValue(password);
         await this.btnSubmit.click();
     }
@@ -34,7 +34,7 @@ class LoginPage extends Page {
      * overwrite specific options to adapt it to page object
      */
     open () {
-        return super.open('login');
+        return super.open('/auth/login');
     }
 }
 
