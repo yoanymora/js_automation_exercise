697fde0be91e8bba90085ea47a75e21bd11cff85
Yoany Mora <yoany.stephany@gmail.com>
2025-08-14 17:39:38 -0600

    [ADD] Base code of Cart page

--- test/pageobjects/cart.page.js
+++ test/pageobjects/cart.page.js
@@ -1,7 +1,7 @@
-import Dashboard from "./dashboard.component";
+import Page from "./page";
 import { $ } from '@/wdio/globals';
 
-class Cart extends Dashboard {
+class Cart extends Page {
     get checkoutButton () {
         return $('button[data-test="proceed-1"]');
     }
